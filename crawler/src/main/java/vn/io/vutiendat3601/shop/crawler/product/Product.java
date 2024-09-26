2fab789bb8fb27918617c2552e832d5bb19be762
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-09-27 02:32:00 +0700

    tmdt-15: crawl data from tiki.vn

---
+++ crawler/src/main/java/vn/io/vutiendat3601/shop/crawler/product/Product.java
@@ -0,0 +1,39 @@
+package vn.io.vutiendat3601.shop.crawler.product;
+
+import java.math.BigDecimal;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Product {
+  private Long id;
+
+  private String sku;
+
+  private String slug;
+
+  private String name;
+
+  private String description;
+
+  @Builder.Default 
+  private BigDecimal unitPrice = new BigDecimal(0D);
+
+  @Builder.Default private BigDecimal unitListedPrice = new BigDecimal(0D);
+
+  private String thumbnail;
+
+  private Long buyedCount;
+
+  private String[] tags;
+
+  private Long likedCount;
+
+  @Builder.Default private Boolean isActive = true;
+
+  private Long unitsInStock;
+
+  private String ref;
+}
