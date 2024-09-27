9ac90ea01aa1faa42c28538ce773dbdfc54383a6
Amory <amory.dev@gmail.com>
2024-09-28 00:54:11 +0700

    tmdt-12: improve makeRandomDigits

--- backend-v2/src/main/java/vn/io/vutiendat3601/shop/v2/util/StringUtils.java
+++ backend-v2/src/main/java/vn/io/vutiendat3601/shop/v2/util/StringUtils.java
@@ -5,10 +5,19 @@ import java.util.Random;
 public class StringUtils {
   private static final String ALPHABET =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+  private static final String DIGITS = "0123456789";
   private static final Random RANDOM = new Random();
 
-  public static String makeRandomDigits(int max) {
-    return "%04d".formatted(RANDOM.nextInt(max + 1));
+  public static String makeRandomDigits(int length) {
+    if (length < 0) {
+      throw new IllegalArgumentException("length must be bigger than or equals to 0");
+    }
+    final int n = DIGITS.length();
+    final StringBuilder result = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      result.append(DIGITS.charAt(RANDOM.nextInt(n)));
+    }
+    return result.toString();
   }
 
   public static String makeRandomString(int length) {
