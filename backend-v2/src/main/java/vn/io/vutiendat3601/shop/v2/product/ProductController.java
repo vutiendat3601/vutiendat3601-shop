d6e7f390a4776c41f2d25cb8f9b239a073acb1b4
Amory <amory.dev@gmail.com>
2024-09-27 20:59:18 +0700

    tmdt-12: fix ProductController

--- backend-v2/src/main/java/vn/io/vutiendat3601/shop/v2/product/ProductController.java
+++ backend-v2/src/main/java/vn/io/vutiendat3601/shop/v2/product/ProductController.java
@@ -2,8 +2,6 @@ package vn.io.vutiendat3601.shop.v2.product;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,21 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
   private final ProductService productService;
 
-  @GetMapping("/v2/categories/{categoryId}/products")
+  @GetMapping("v2/categories/{categoryId}/products")
   public ResponseEntity<List<ProductDto>> getProductsByCategoryId(@PathVariable Long categoryId) {
     final List<ProductDto> productDtos = productService.getProductsByCategoryId(categoryId);
     return ResponseEntity.ok(productDtos);
-  }
-
-  @GetMapping("/v2/products")
-  public ResponseEntity<List<ProductDto>>  getAllProducts(){
-    final List<ProductDto> productDtos = productService.getAllProducts();
-    return ResponseEntity.ok(productDtos);
-  }
-
-  @GetMapping("/v2/products/{id}")
-  public ResponseEntity<Optional<ProductDto>> getProductById(@PathVariable("id") Long id){
-    final Optional<ProductDto> prOptional = productService.getProductById(id);
-    return ResponseEntity.ok(prOptional);
   }
 }
