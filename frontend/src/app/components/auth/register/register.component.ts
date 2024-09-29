39d39b476e0c8722ed285e14af177fd854749df5
htphatz <huynhphatsn@gmail.com>
2024-09-29 23:30:19 +0700

    tmdt-14: design Register page and Validate input data

--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+        terms: [false, Validators.requiredTrue],
+      },
+      { validator: this.checkPasswords }
+    );
+  }
+
+  ngOnInit(): void {}
+
+  checkPasswords(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notSame: true };
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Thông tin đăng ký hợp lệ', this.registerForm.value);
+    } else {
+      console.log('Thông tin đăng ký không hợp lệ');
+    }
+  }
 }
