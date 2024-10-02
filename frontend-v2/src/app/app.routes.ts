8d9c3557d11e2495e14b6571c34cf89e6c07c5f9
htphatz <huynhphatsn@gmail.com>
2024-10-02 22:53:59 +0700

    tmdt-22: design Auth pages

--- frontend-v2/src/app/app.routes.ts
+++ frontend-v2/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { ProductListComponent } from './components/product-list/product-list/product-list.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SIGNAL } from '@angular/core/primitives/signals';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 export const routes: Routes = [
   // { path: 'checkout', component: CheckoutComponent },
@@ -19,5 +24,24 @@ export const routes: Routes = [
     path: '',
     component: MainLayoutComponent,
     children: [{ path: '', component: ProductListComponent }],
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{ path: '', component: CartDetailsComponent }],
   },
 ];
