18075cd009824b2a4d2264770a62edd99acc9721
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-10-02 23:30:46 +0700

    tmdt-22: modify OpenAPIDefinition config

--- backend-v2/src/main/java/vn/io/vutiendat3601/shop/v2/common/OpenApiConfig.java
+++ backend-v2/src/main/java/vn/io/vutiendat3601/shop/v2/common/OpenApiConfig.java
@@ -3,14 +3,16 @@ package vn.io.vutiendat3601.shop.v2.common;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 // import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
     servers = {
-      // @Server(url = "http://localhost:8000", description = "local"),
+      @Server(url = "https://apidev.shopsinhvien.io.vn", description = "dev"),
+      @Server(url = "http://localho.st:8080", description = "local"),
     },
     info =
         @Info(
-            title = "vutiendat3601 Shop APIs",
+            title = "Shop Sinh Vien APIs",
             version = "2.0",
-            description = "vutiendat3601 Shop APIs"))
+            description = "Shop Sinh Vien Shop APIs version 2"))
 public class OpenApiConfig {}
