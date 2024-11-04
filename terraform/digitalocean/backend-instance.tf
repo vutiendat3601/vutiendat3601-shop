2903ae352411b5e7ef388473396afdbe6e145a6d
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-11-04 19:09:03 +0700

    tmdt-39: delete redundant files

--- terraform/digitalocean/backend-instance.tf
+++
@@ -1,7 +0,0 @@
-
-resource "digitalocean_droplet" "backend-v2" {
-  image  = "ubuntu-20-04-x64"
-  name   = "backend-v2"
-  region = "sgp2"
-  size   = "s-1vcpu-1gb"
-}
--- terraform/digitalocean/postgres-cluster.tf
+++
@@ -1,8 +0,0 @@
-resource "digitalocean_database_cluster" "postgres-cluster" {
-  name       = "postgres-cluster"
-  engine     = "pg"
-  version    = "16"
-  size       = "db-s-1vcpu-1gb"
-  region     = "sgp1"
-  node_count = 1
-}
