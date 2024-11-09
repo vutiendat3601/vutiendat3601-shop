cb8eb0722b9c345a00e6e79d1432d96773090e31
Nguyễn Quang Trung Nhân <127121102+trungnhanforwork@users.noreply.github.com>
2024-11-09 11:16:04 +0700

    tmdt-40: configure Testcontainers (#113)

---
+++ backend-v2/src/test/java/vn/io/vutiendat3601/shop/v2/testcontainers/AbstractTestcontainersTest.java
@@ -0,0 +1,43 @@
+package vn.io.vutiendat3601.shop.v2.testcontainers;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@DirtiesContext
+public abstract class AbstractTestcontainersTest {
+    @Container
+    protected static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
+
+    static {
+        POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:16-alpine3.20");
+        POSTGRES_CONTAINER.withDatabaseName("shop");
+        POSTGRES_CONTAINER.withUsername("shop");
+        POSTGRES_CONTAINER.withPassword("123456Aa@");
+    }
+
+    @DynamicPropertySource
+    static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        final Flyway flyway =
+                Flyway.configure()
+                        .dataSource(
+                                POSTGRES_CONTAINER.getJdbcUrl(),
+                                POSTGRES_CONTAINER.getUsername(),
+                                POSTGRES_CONTAINER.getPassword())
+                        .load();
+        flyway.migrate();
+    }
+}
